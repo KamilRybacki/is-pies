54c939248caa92ed5eb7b2bb48656b4d8746e2f5
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-12-09 10:22:08 +0100

    save

--- src/tbp/detect.rs
+++ src/tbp/detect.rs
@@ -1,6 +1,6 @@
-use std::{env, iter::Map};
+use std::{env, f32::consts::E};
 use serde::Deserialize;
-use reqwest::{Client, header::AUTHORIZATION};
+use reqwest::{Client, header::AUTHORIZATION, Error};
 
 const LANGUAGE_DETECTION_URL: &str =  "https://ws.detectlanguage.com/0.2/detect";
 const DETECTION_CLIENT: Client = Client::new();
@@ -19,28 +19,34 @@ struct LanguageDetectionResponse {
   data: Vec<LanguageDetection>,
 }
 
-async fn detect_language(text: &str) -> Result<Vec<&str>, &str> {
+async fn detect_language(text: &str) -> Result<Vec<&str>, Vec<&str>> {
   let api_key = env::var("LANGUAGE_DETECTION_API_KEY")
     .unwrap_or_default();
   if api_key.len() == 0 {
-    return Err("Please set the LANGUAGE_DETECTION_API_KEY environment variable");
+    return Err(Vec::new());
   }
   let detection_request_url = format!("{}?q={}", LANGUAGE_DETECTION_URL, text);
-  let response = DETECTION_CLIENT
+  let response = match DETECTION_CLIENT
     .get(&detection_request_url)
     .header(AUTHORIZATION, format!("Bearer {}", api_key))
     .send()
-    .await?
-    .json::<LanguageDetectionResponse>();
+    .await {
+      Ok(response) => response.json::<LanguageDetectionResponse>(),
+      Err(_) => return Err(Vec::new()),
+    }
+    .await
+    .unwrap()
+    .data;
 
-  let language_detection = response.await?;
-  let detected_languages = language_detection.data
+  if response.len() == 0 {
+    return Err(Vec::new());
+  }
+
+  let detected_languages = response
     .iter()
     .map(|detection| detection.language.as_str())
-    .collect::<Vec<&str>>();
-  if detected_languages.len() == 0 {
-    println!("No languages detected");
-    return Err("No languages detected");
-  }
+    .collect::<Vec<&str>>()
+    .clone();
+
   return Ok(detected_languages);
 }
